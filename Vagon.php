8fc1b7648f47cf24f7624eea964941293f7a0e4b
Roxana-Munoz <135390411+Roxana-Munoz@users.noreply.github.com>
2025-05-22 20:42:54 -0300

    Update Vagon.php

--- Vagon.php
+++ Vagon.php
@@ -11,59 +11,65 @@ class Vagon{
     private $largoVagon;
     private $anchoVagon;
     private $pesoVagonVacio;
-    
+    private $pesoTotal;
+
     //METODO CONSTRUCTOR
     public function __construct($anioInstalacion,$largoVagon,$anchoVagon,$pesoVagonVacio){
         $this->anioInstalacion=$anioInstalacion;
         $this->largoVagon=$largoVagon;
         $this->anchoVagon=$anchoVagon;
         $this->pesoVagonVacio=$pesoVagonVacio;
-       
-
-    //METODOS GETERS
+        $this->pesoTotal=$pesoVagonVacio;
+        }   
     public function getAnioInstalacion(){
         return $this->anioInstalacion;
     } 
-     public function getLargoVagon(){
+    public function getLargoVagon(){
         return $this->largoVagon;
     } 
     
     public function getAnchoVagon(){
         return $this->anchoVagon;
     } 
-     public function getPesoVagonVacio(){
+    public function getPesoVagonVacio(){
         return $this->pesoVagonVacio;
 
     } 
- 
+    public function getPesoTotal(){
+        return $this->pesoTotal;
+
+    } 
     //METODOS SETERS 
     public function setAnioInstalacion($anioInstalacion){
-     $this->anioInstalacion=$anioInstalacion;   
+        $this->anioInstalacion=$anioInstalacion;   
     }
     public function setLargoVagon($largoVagon){
         $this->largoVagon=$largoVagon;
 
-     }   
-      public function setAnchoVagon($anchoVagon){
-         $this->anchoVagon=$anchoVagon;
-      }
-     public function setPesoVagonVacio($pesoVagonVacio){
+        }   
+    public function setAnchoVagon($anchoVagon){
+            $this->anchoVagon=$anchoVagon;
+     }
+    public function setPesoVagonVacio($pesoVagonVacio){
         $this->pesoVagonVacio=$pesoVagonVacio;
 
-     }
-     
+    }
+    public function setPesoTotal($pesoTotal){
+        $this->pesoTotal=$pesoTotal;
+
+    }
      //METODO __TO STRING
-     public function __toString()
-     {
+    public function __toString()
+    {
         $mensaje=" Año de instalacion del vagon: ". $this->getAnioInstalacion()."\n";
-                 "Largo del vagon: ". $this->getLargoVagon()." mts"."\n";
-                 "Ancho del vagon: ". $this->getAnchoVagon()." mts"."\n";
-                 "Peso del vagon vacio: ". $this->getPesoVagonVacio()." kg"."\n";;
-                   
+        $mensaje .="Largo del vagon: ". $this->getLargoVagon()."\n";
+        $mensaje .="Ancho del vagon: ". $this->getAnchoVagon()."\n";
+        $mensaje .= "Peso del vagon vacio: ". $this->getPesoVagonVacio()." kg"."\n";
+        $mensaje .= "Peso total del vagon: ". $this->getPesoTotal()." kg"."\n";
                 return $mensaje;
-     }
+    }
     public function calcularPesoVagon() {
-    // devuelvo el peso del vagón vacío
+    // devuelve solo el peso del vagón vacío*/
         return $this->getPesoVagonVacio();
     }
 
